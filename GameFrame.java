472e9de1e5d705d40f0f1ff02bdfe4fedd367561
Ismail Sifdine <131058788+Sif247@users.noreply.github.com>
2024-03-10 17:55:05 +0100

    Add files via upload

---
+++ GameFrame.java
@@ -0,0 +1,17 @@
+package Gioco;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+    GameFrame()
+    {
+        this.add(new GamePanel());
+
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+}
